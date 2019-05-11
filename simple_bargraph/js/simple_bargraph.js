0546c50b693ed8823f2c42fbb8a2a3592dd5a856
Robert Oikle <6431161+roikle@users.noreply.github.com>
2019-05-10 20:31:59 -0400

    Reversed direction of graph bars

--- simple_bargraph/js/simple_bargraph.js
+++ simple_bargraph/js/simple_bargraph.js
@@ -1,27 +1,33 @@
 
 var sample_data = [8.3,5.0,2.1,0.0,0.0,0.0,3.2,6.8,9.1,0.0,1.1,7.9,6.2,2.0,0.0,0.0,0.0,0.0,5.0,5.2,2.1];
 var svg = d3.select("svg");
-var x = 10;
-var y = 0;
-var barwidth = 10;
+var graph = {
+	svgHeight: 500,
+	svgWidth: 500,
+	barWidth: 10,
+	fillColour: "#2a7f64",
+	x: 10,
+	y: 0
+};
 
 // Modify the svg 
-svg.attr('width', '500px')
-	.attr('height', '500px')
-	.classed('bargraph', true)
-	.style('background-color', 'red');
+svg.attr('width', graph.svgWidth + 'px')
+	.attr('height', graph.svgHeight + 'px')
+	.classed('bargraph', true);
 
 // Add a bar for each of the sample data values
 svg.append('g')
-	.attr('fill', 'green')
+	.attr('fill', graph.fillColour)
 	.selectAll('rect')
 	.data(sample_data)
 	.join('rect')
 	.attr('x', function(d, i){
-		return x * i;
+		return graph.x * i;
 	})
-	.attr('y', y)
-	.attr('width', barwidth)
+	.attr('y', function(d){
+		return graph.svgHeight - d;
+	})
+	.attr('width', graph.barWidth)
 	.attr('height', function(d){
-		return d * 10;
+		return d;
 	});
