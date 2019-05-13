83ef70ca8df8dbdf73dff80eccc4c7b139b1c7ce
Robert Oikle <6431161+roikle@users.noreply.github.com>
2019-05-12 21:55:28 -0400

    Scaled sample data

--- simple_bargraph/js/simple_bargraph.js
+++ simple_bargraph/js/simple_bargraph.js
@@ -3,13 +3,19 @@ class simpleBarGraph {
 	createGraph() {
 		const height = 500;
 		const width = 500;
+		const fillColour = "#2A7F64";
+
 		let sample_data = [8.3,5.0,2.1,0.0,0.0,0.0,3.2,6.8,9.1,0.0,1.1,7.9,6.2,2.0,0.0,0.0,0.0,0.0,5.0,5.2,2.1];
-		let graph = {
-			barWidth: 10,
-			fillColour: "#2a7f64",
-			x: 10,
-			y: 0
-		};
+
+		let bandWidth = width / sample_data.length;
+
+		let scaleX = d3.scaleLinear()
+			.domain([d3.min(sample_data), d3.max(sample_data)])
+			.range([0, width]);
+	
+		let scaleY = d3.scaleLinear()
+			.domain([d3.min(sample_data), d3.max(sample_data)])
+			.range([height, 0]);
 
 		let svg = d3.select("svg");
 
@@ -20,19 +26,19 @@ class simpleBarGraph {
 		
 		// Add a bar for each of the sample data values
 		svg.append('g')
-			.attr('fill', graph.fillColour)
+			.attr('fill', fillColour)
 			.selectAll('rect')
 			.data(sample_data)
 			.join('rect')
-			.attr('x', function(d, i){
-				return graph.x * i;
+			.attr('x', function(d,i){
+				return i * bandWidth;
 			})
 			.attr('y', function(d){
-				return height - d;
+				return scaleY(d);
 			})
-			.attr('width', graph.barWidth)
+			.attr('width', bandWidth)
 			.attr('height', function(d){
-				return d;
+				return height - scaleY(d);
 			});
 	}
 }
