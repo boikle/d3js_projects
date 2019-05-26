84fc3b7b257d6a5c77e019caf9ec14fe5a8921b8
Robert Oikle <6431161+roikle@users.noreply.github.com>
2019-05-26 11:21:08 -0400

    Added sample data

--- simple_graphs/simple_piechart/js/simple_piechart.js
+++ simple_graphs/simple_piechart/js/simple_piechart.js
@@ -1,3 +1,11 @@
+const col_sample_data = [
+	{"category": "rent", "cost": 2000},
+	{"category": "transportation", "cost": 200},
+	{"category": "power", "cost": 100 },
+	{"category": "water", "cost": 10},
+	{"category": "food", "cost": 300}
+];
+
 const margin = {
 	"top": 20,
 	"right": 20,
@@ -5,15 +13,18 @@ const margin = {
 	"left": 30
 };
 
-const height = 400 - margin.top - margin.bottom;
-const width = 800 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
+const width = 500 - margin.left - margin.right;
 let svg = d3.select('svg');
 
-function drawpiechart(){
-	// set dimensions for svg
+function setSVGDimensions(){
 	svg.classed('piechart', true)
 		.attr('width', (width + margin.left + margin.right) + 'px')
 		.attr('height', (height + margin.top + margin.bottom) + 'px');
+};
+
+function drawpiechart(){
+	setSVGDimensions();
 };
 
 export { drawpiechart };
