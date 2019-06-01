81b3587ef648899b921f84cc12a5985f892edde1
Robert Oikle <6431161+roikle@users.noreply.github.com>
2019-06-01 17:20:36 -0400

    Added points to scatter plot

--- simple_graphs/simple_scatterplot/js/random_point_generator.js
+++ simple_graphs/simple_scatterplot/js/random_point_generator.js
@@ -2,12 +2,13 @@
 // Option: sampleSize - defines the dataset size (default = 50)
 function randomPointGenerator(sampleSize = 50) {
 	let i, x, y;
+	let max = 100;
 	let dataset = [];
 
 	if (sampleSize > 0) {
 		for (i = 0; i < sampleSize; i += 1){
-			x = Math.floor(Math.random() * 100);
-			y = Math.floor(Math.random() * 100);
+			x = Math.floor(Math.random() * max);
+			y = Math.floor(Math.random() * max);
 			dataset.push({"x": x, "y": y});
 		}
 	}
--- simple_graphs/simple_scatterplot/js/simple_scatterplot.js
+++ simple_graphs/simple_scatterplot/js/simple_scatterplot.js
@@ -7,45 +7,36 @@ const margin = {
 	"left": 30
 }
 
+const min = 0; 
+const max = 100;
 const height = 400 - margin.top - margin.bottom;
 const width = 800 - margin.left - margin.right;
+const radius = 5;
 
 let data = randomPointGenerator()
 
-function getXValue(d){
-	if (d.hasOwnProperty('x')) {
-		return d.x;
-	}
-}
+let svg = d3.select("svg")
+	.attr('width', (width + margin.left + margin.right) + 'px')
+	.attr('height', (height + margin.top + margin.bottom) + 'px')
+	.classed('scatter-plot', true);
 
-function getYValue(d){
-	if (d.hasOwnProperty('y')) {
-		return d.y;
-	}
-}
+let scaleX = d3.scaleLinear()
+	.domain([min, max])
+	.range([0, width]);
+
+let scaleY = d3.scaleLinear()
+	.domain([min, max])
+	.range([height, 0]);
+
+let xAxis = g => g
+	.attr('transform', 'translate(' + margin.left + ',' + (height + margin.top) + ')')
+	.call(d3.axisBottom().ticks(10).scale(scaleX));
+
+let yAxis = g => g
+	.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+	.call(d3.axisLeft().ticks(5).scale(scaleY));
 
 function draw(){ 
-	let svg = d3.select("svg")
-		.attr('width', (width + margin.left + margin.right) + 'px')
-		.attr('height', (height + margin.top + margin.bottom) + 'px')
-		.classed('scatter-plot', true);
-	
-	let scaleX = d3.scaleLinear()
-		.domain([d3.min(data, getXValue), d3.max(data, getYValue)])
-		.range([0, width]);
-	
-	let scaleY = d3.scaleLinear()
-		.domain([d3.min(data, getXValue), d3.max(data, getYValue)])
-		.range([height, 0]);
-	
-	let xAxis = g => g
-		.attr('transform', 'translate(' + margin.left + ',' + (height + margin.bottom) + ')')
-		.call(d3.axisBottom().ticks(10).scale(scaleX));
-	
-	let yAxis = g => g
-		.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-		.call(d3.axisLeft().ticks(5).scale(scaleY));
-	
 	// Add x axis
 	svg.append('g')
 		.call(xAxis);
@@ -53,6 +44,27 @@ function draw(){
 	// Add y axis
 	svg.append('g')
 		.call(yAxis);
+
+	// Add points to graph
+	let points = svg.selectAll('circle')
+		.data(data);
+
+	points.enter()
+		.append('circle')
+		.merge(points)
+		.transition()
+		.attr('r', radius)
+		.attr('cx', function(d){
+			return scaleX(d.x) + margin.left;
+		})
+		.attr('cy', function(d){
+			return scaleY(d.y) + margin.top ;
+		})
+		.attr('fill', 'darkcyan')
+		.attr('stroke', 'black')
+		.attr('datat', function(d){
+			return '['+d.x+' ,'+d.y+']';	
+		});
 }
 
 export { draw };
