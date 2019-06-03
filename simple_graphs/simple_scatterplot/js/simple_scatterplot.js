c099c31f4cad6a11079b7f6c045668057fdcf13b
Robert Oikle <6431161+roikle@users.noreply.github.com>
2019-06-02 20:09:31 -0400

    Added point labels to scatterplot

--- simple_graphs/simple_scatterplot/js/simple_scatterplot.js
+++ simple_graphs/simple_scatterplot/js/simple_scatterplot.js
@@ -11,7 +11,8 @@ const min = 0;
 const max = 100;
 const height = 400 - margin.top - margin.bottom;
 const width = 800 - margin.left - margin.right;
-const radius = 5;
+const radius = 3;
+const labelPadding = 3;
 
 let data = randomPointGenerator()
 
@@ -19,6 +20,12 @@ let svg = d3.select("svg")
 	.attr('width', (width + margin.left + margin.right) + 'px')
 	.attr('height', (height + margin.top + margin.bottom) + 'px')
 	.classed('scatter-plot', true);
+	
+// Add groups for points and labels
+let pointsGroup = svg.append('g')
+	.classed('points', true);
+let labelsGroup = svg.append('g')
+	.classed('labels', true);
 
 let scaleX = d3.scaleLinear()
 	.domain([min, max])
@@ -45,8 +52,8 @@ function draw(){
 	svg.append('g')
 		.call(yAxis);
 
-	// Add points to graph
-	let points = svg.selectAll('circle')
+	// Add points
+	let points = pointsGroup.selectAll('circle')
 		.data(data);
 
 	points.enter()
@@ -58,13 +65,34 @@ function draw(){
 			return scaleX(d.x) + margin.left;
 		})
 		.attr('cy', function(d){
-			return scaleY(d.y) + margin.top ;
+			return scaleY(d.y) + margin.top;
 		})
 		.attr('fill', 'darkcyan')
 		.attr('stroke', 'black')
 		.attr('datat', function(d){
 			return '['+d.x+' ,'+d.y+']';	
 		});
+
+
+	let labels = labelsGroup.selectAll('text')
+		.data(data);
+
+	labels.enter()
+		.append('text')
+		.merge(labels)
+		.transition()
+	    .attr("font-family", "sans-serif")
+      	.attr("font-size", 11)
+		.attr('x', function(d){
+			return scaleX(d.x) + margin.left + labelPadding;
+		})
+		.attr('y', function(d){
+			return scaleY(d.y) + margin.top - labelPadding;
+		})
+		.text(function(d){
+			return d.x + ', ' + d.y;
+		});
+
 }
 
 export { draw };
